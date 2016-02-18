1724c5cc5b0e0be2195b43840c950f6c4fd59297
RomainTreppoz <romaintreppoz@gmail.com>
2016-02-18 17:05:03 +0100

    Jeudi soir

--- app/routes.php
+++ app/routes.php
@@ -3,4 +3,7 @@
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
 		['GET', '/publier', 'Publish#index', 'publier'],
+		['GET', '/publier/', 'Publish#index', 'publier2'],
+		['GET', '/publiyer/', 'Publish#index', 'publier3'],
+
 	);
