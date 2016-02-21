43bcbef2e85b9504d6e685ed3288d56faebba581
RomainTreppoz <romaintreppoz@gmail.com>
2016-02-21 19:22:36 +0100

    ajout d'une image fichier erreur 404.php

--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,5 +1,6 @@
 <?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>404. Perdu ?</h1>
+<h1>404. Perdu ? Probablement une erreur 404 !</h1>
+<img src="<?= $this->assetUrl("img/404.jpg")?>"class="mimic img-responsive" alt="erreur 404">
 <?php $this->stop('main_content'); ?>
