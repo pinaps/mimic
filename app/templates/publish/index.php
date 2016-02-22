f30b6d40e25aa4cf1922c5fe027f42daa857830c
Pinaps <ageant57@gmail.com>
2016-02-22 10:40:55 +0100

    Comit

--- app/templates/publish/index.php
+++ app/templates/publish/index.php
@@ -5,58 +5,51 @@
   <form method="POST" action="<?= $this->url('publierPost'); ?>">
     <div class="row">
 
-      <div class="col-md-12">
-        <h2>Poste tes photos et/ou tes textes</h2>
+      <div class="col-md-12" id="mainBoxFaire">
+        <h2 id="titleMain">Crée ta propre Story !</h2>
         <!-- = remplace le php echo -->
-        <div class="col-md-3">
+        <div class="col-md-4">
           <img src="<?= $this->assetUrl("img/donne_ta_photo.jpg")?>"class="mimic img-responsive" name="texte1" alt="donne ta photo">
 
-          <div class="form-group">
+          <div class="form-group  ">
             <label for="exampleInputFile">Première photo</label>
             <input type="file" id="exampleInputFile">
-            <p class="help-block">Insére la première photo</p>
+           
           </div>
+          <!--  TEXTE 1 -->
+           <textarea class="form-control mimic" id="lol" rows="5" name="texte1" maxlength="255" placeholder="J'écris mon intro ici"></textarea>
 
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
           <img src="<?= $this->assetUrl("img/donne_ta_photo.jpg")?>" class="mimic img-responsive" name="texte2" alt="donne ta photo">
 
           <div class="form-group">
             <label for="exampleInputFile">Deuxième photo</label>
             <input type="file" id="exampleInputFile">
-            <p class="help-block">Insére la deuxième photo</p>
+            
           </div>
+          <!-- TEXTE 2 -->
+          <textarea class="form-control mimic" rows="5"  id="lol"name="texte2" maxlength="255" placeholder="Et ici la 2e partie de l'histoire"></textarea>
 
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
           <img src="<?= $this->assetUrl("img/donne_ta_photo.jpg")?>" class="mimic img-responsive" name="texte3" alt="donne ta photo">
 
           <div class="form-group">
             <label for="exampleInputFile">Troisième photo</label>
             <input type="file" id="exampleInputFile">
-            <p class="help-block">Insére la troisième photo</p>
+            
           </div>
+          <!-- TEXTE 3 -->
+
+          <textarea class="form-control mimic" rows="5" id="lol" name="texte3" maxlength="255" placeholder="Et enfin la chute :D"></textarea>
+
 
         </div>
-
-
-        <div class="col-md-12">
-
-          <div class="col-md-3">
-            <textarea class="form-control mimic" rows="5" name="texte1" maxlength="255" placeholder="Insére le premier texte"></textarea>
-          </div>
-
-          <div class="col-md-3">
-            <textarea class="form-control mimic" rows="5" name="texte2" maxlength="255" placeholder="Insére le premier texte"></textarea>
-          </div>
-
-          <div class="col-md-3">
-            <textarea class="form-control mimic" rows="5" name="texte3" maxlength="255" placeholder="Insére le premier texte"></textarea>
-          </div>                        
-
-          <button type="submit" class="btn btn-default btn-lg">
-            <span class="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span> Envoyer !
+          <button type="submit" class="btn btn-danger btn-lg">
+            <span class="glyphicon glyphicon-send" aria-hidden="true"></span> Envoyer !
           </button>
+      
 
         </div>
 
